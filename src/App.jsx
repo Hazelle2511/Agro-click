f5605bef148ff3e4349d17913654ebf90c3f8673
Hazelle2511 <camacho.hazelle@gmail.com>
2021-04-30 14:09:45 +0200

    deployment in netlify

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+export default () => {
+    //create the count state
+    const [count, setCount] = useState(0);
+
+    //update the count (+1 every second)
+
+    useEffect(()=> {
+        const timer = setTimeout(()=>setCount(count + 1), 1000);
+        return () => clearTimeout(timer);
+    }, [count, setCount]);
+
+    //Return the App Component
+    return(
+        <div className = "App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Hello HRM Refresh, Page has been open for <code>{count}</code> times
+                </p>
+
+            </header>
+
+        </div>
+    )
+}
