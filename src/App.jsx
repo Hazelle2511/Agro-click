7ee094462554e983a7259631e684b051d4a48783
Hazelle2511 <camacho.hazelle@gmail.com>
2021-04-30 14:29:39 +0200

    development base ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import React, {Component,useState, useEffect} from 'react';
+import './App.css'
+import logo from './logo.svg';
+
 export default () => {
     //create the count state
     const [count, setCount] = useState(0);
