86310b81dbee94ef22e3f64000dfa653a94b12e9
Anais <anais.hammouche@edu.itescia.fr>
2021-05-03 15:10:00 +0200

    clean

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  // purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html']
-  purge: ["./src/**/*.html"],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -10,19 +9,3 @@ module.exports = {
   },
   plugins: [],
 }
-
-
-// const defaultTheme = require('tailwindcss/defaultTheme');
-
-// module.exports = {
-//   purge: ['./src/**/*.jsx', './src/**/*.js'],
-//   theme: {
-//     extend: {
-//       fontFamily: {
-//         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-//       },
-//     },
-//   },
-//   variants: {},
-//   plugins: [],
-// };
