451196c118991bed7c12716c6f43ef89bee725e0
Hazelle2511 <camacho.hazelle@gmail.com>
2021-05-03 17:14:02 +0200

    conflit

--- snowpack.config.js
+++ snowpack.config.js
@@ -5,8 +5,7 @@ module.exports = {
     public: '/',
     src: '/dist',
   },
-    plugins: ['@snowpack/plugin-react-refresh', "@snowpack/plugin-postcss"],
-     
+    plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-postcss'],
 
   routes: [
     /* Enable an SPA Fallback in development: */
