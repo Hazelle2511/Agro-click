82603e749a8c1b155a4d7a262b18ffc7c4f910c0
Hazelle2511 <camacho.hazelle@gmail.com>
2021-05-05 12:31:57 +0200

    Accueil reglé

--- src/Components/Accueil.jsx
+++ src/Components/Accueil.jsx
@@ -31,75 +31,42 @@ class Accueil extends Component {
    </video>
 
 </header>
-<div className="flex items-center justify-between leading-tight p-2 md:p-4">
-  <h1 className="text-lg">
-   <div className="no-underline hover:underline text-black" href="#">
-       Provence-Alpes-Côte d’Azur
-   </div> 
-   </h1> 
-   <p className="text-grey-darker text-sm">
-       Avignon
-   </p>
+<div className="container my-12 mx-auto px-4 md:px-12">
+<div className="flex flex-wrap -mx-1 lg:-mx-4">
+<div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+<div class="overflow-hidden rounded-lg shadow-lg">
+
+      <Link to="">
+            <img alt="Placeholder" class="block h-auto w-full" src="https://picsum.photos/600/400/?random">
+            </img>
+      </Link>   
+      <div className="flex items-center justify-between leading-tight p-2 md:p-4">
+        <h1 className="text-lg">
+        <div className="no-underline hover:underline text-black" href="#">
+            Provence-Alpes-Côte d’Azur
+        </div> 
+        </h1> 
+        <p className="text-grey-darker text-sm">
+            Avignon
+        </p>
+      </div>
+
+      <div className="flex items-center justify-between leading-none p-2 md:p-4">
+      <Link className="flex items-center no-underline hover:underline text-black" href="#">
+        <img alt="Placeholder" className="block rounded-full" src="https://picsum.photos/32/32/?random"></img>
+        <p className="ml-2 text-sm">Le fermier du sud</p>
+      </Link>
+      <Link className="no-underline text-grey-darker hover:text-red-dark" href="#">
+        <div className="hidden">Like</div>
+        <div className="fa fa-heart"></div>
+      </Link>
+
+      </div>
 </div>
-
-<div className="flex items-center justify-between leading-none p-2 md:p-4">
-<Link className="flex items-center no-underline hover:underline text-black" href="#">
-  <img alt="Placeholder" className="block rounded-full" src="https://picsum.photos/32/32/?random"></img>
-  <p className="ml-2 text-sm">Le fermier du sud</p>
-</Link>
-<Link className="no-underline text-grey-darker hover:text-red-dark" href="#">
-  <div className="hidden">Like</div>
-  <div className="fa fa-heart"></div>
-</Link>
-
 </div>
-</>
-
-  //             <footer className="flex items-center justify-between leading-none p-2 md:p-4">
-    //                 <Link className="flex items-center no-underline hover:underline text-black" href="#">
-    //                     <img alt="Placeholder" className="block rounded-full" src="https://picsum.photos/32/32/?random"></img><p className="ml-2 text-sm">
-    //                         Le fermier du sud
-    //                     </p></Link>
-    //                     <Link className="no-underline text-grey-darker hover:text-red-dark" href="#">
-    //                     <div className="hidden">Like</div>
-    //                     <div className="fa fa-heart"></div>
-    //                 </Link></footer>
-     
- 
-
-    //     <div className="container my-12 mx-auto px-4 md:px-12">
-    // <div className="flex flex-wrap -mx-1 lg:-mx-4">
-    // <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-    // <div class="overflow-hidden rounded-lg shadow-lg">
-      
-    //   <Link to="">
-    //   <img alt="Placeholder" class="block h-auto w-full" src="https://picsum.photos/600/400/?random">
-    //   </img></Link>   
-    //   <header className="flex items-center justify-between leading-tight p-2 md:p-4">
-    //                 <h1 className="text-lg">
-    //                     <div className="no-underline hover:underline text-black" href="#">
-    //                     Provence-Alpes-Côte d’Azur
-    //                     </div> </h1> <p className="text-grey-darker text-sm">
-    //                     Avignon
-    //                 </p>
-    //             </header>
-    //             <footer className="flex items-center justify-between leading-none p-2 md:p-4">
-    //                 <Link className="flex items-center no-underline hover:underline text-black" href="#">
-    //                     <img alt="Placeholder" className="block rounded-full" src="https://picsum.photos/32/32/?random"></img><p className="ml-2 text-sm">
-    //                         Le fermier du sud
-    //                     </p></Link>
-    //                     <Link className="no-underline text-grey-darker hover:text-red-dark" href="#">
-    //                     <div className="hidden">Like</div>
-    //                     <div className="fa fa-heart"></div>
-    //                 </Link></footer>
-    //   </div>              
-    //   </div>
-    //   </div>
-    //   </div>
-
-    //   </div>
-
-     
+</div>
+</div>
+</>  
       )
 
   
