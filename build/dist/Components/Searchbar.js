e9c02bcc863b4b5824e9bd6ddc20289b6e95a5b3
Hazelle2511 <camacho.hazelle@gmail.com>
2021-05-19 17:12:40 +0200

    trying to solve redirects

---
+++ build/dist/Components/Searchbar.js
@@ -0,0 +1,18 @@
+import React from "../../_snowpack/pkg/react.js";
+function Searchbar() {
+  return /* @__PURE__ */ React.createElement("div", {
+    className: "p-2"
+  }, /* @__PURE__ */ React.createElement("div", {
+    className: "bg-white flex items-center rounded-full shadow-sm"
+  }, /* @__PURE__ */ React.createElement("input", {
+    className: "rounded-l-full w-full py-1 px-1 text-gray-700 leading-tight focus:outline-none ",
+    id: "search",
+    type: "text",
+    placeholder: "Rechercher"
+  }), /* @__PURE__ */ React.createElement("div", {
+    className: "p-2"
+  }, /* @__PURE__ */ React.createElement("button", {
+    className: "bg-yellow-500 text-white rounded-full p-1 hover:bg-yellow-400 focus:outline-none w-6 h-6 flex items-center justify-center"
+  }))));
+}
+export default Searchbar;
